ed820254510ac0aa7364290f01982546ecd95ec4
Lukas Rutkauskas <lukas.rutkauskas@stud.techin.lt>
2023-11-21 13:16:10 +0200

    added arrays to hold images and proverbs

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-let img = []; // An array holding at least 5 pictures 
-let proverb = []; // An array holding at least 5 proverbs
+ // An array holding at least 5 pictures 
+ // An array holding at least 5 proverbs
 
 function randomIMG (img) {
-    
+    // Array storing images
+    var randomImage = new Array();  
+    randomImage[0] = "";
+    randomImage[1] = "";
+    randomImage[2] = "";
+    randomImage[3] = "";
+    randomImage[4] = "";
+
 }
 function randomProverb (proverb) {
-
+    // Array storing proverbs
+    var randomProverb = new Array();  
+    randomProverb[0] = "";
+    randomProverb[1] = "";
+    randomProverb[2] = "";
+    randomProverb[3] = "";
+    randomProverb[4] = "";
 }
