bcc413318affc39adeefb35704b5fca65d198d84
Lukas Rutkauskas <lukas.rutkauskas@stud.techin.lt>
2023-11-21 13:09:52 +0200

    fixed comments

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let img = []; // An array holding at least 10 pictures 
-let proverb = []; // An array holding at least 10 proverbs
+let img = []; // An array holding at least 5 pictures 
+let proverb = []; // An array holding at least 5 proverbs
 
 function randomIMG (img) {
     
