c99874e8cc1da1613e4df08788d7942e9fa3198a
elvinas grimm <elvinas.zaleskas@stud.techin.lt>
2023-11-21 13:25:26 +0200

    added proverbs and images

--- main.js
+++ main.js
@@ -4,19 +4,19 @@
  function randomIMG (img) {
     // Array storing images
     var randomImage = new Array();  
-    randomImage[0] = "";
-    randomImage[1] = "";
-    randomImage[2] = "";
-    randomImage[3] = "";
-    randomImage[4] = "";
+    randomImage[0] = "https://images.pexels.com/photos/36762/scarlet-honeyeater-bird-red-feathers.jpg";
+    randomImage[1] = "https://images.pexels.com/photos/247431/pexels-photo-247431.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+    randomImage[2] = "https://images.pexels.com/photos/1322185/pexels-photo-1322185.jpeg";
+    randomImage[3] = "https://images.pexels.com/photos/34950/pexels-photo.jpg";
+    randomImage[4] = "https://images.pexels.com/photos/40896/larch-conifer-cone-branch-tree-40896.jpeg";
 
 }
 function randomProverb (proverb) {
     // Array storing proverbs
     var randomProverb = new Array();  
-    randomProverb[0] = "";
-    randomProverb[1] = "";
-    randomProverb[2] = "";
-    randomProverb[3] = "";
-    randomProverb[4] = "";
+    randomProverb[0] = "Beauty is in the eye of the beholder";
+    randomProverb[1] = "Beggars can’t be choosers";
+    randomProverb[2] = "Don’t bite the hand that feeds you";
+    randomProverb[3] = "Don’t judge a book by its cover";
+    randomProverb[4] = "Don’t put all of your eggs in one basket";
 }
