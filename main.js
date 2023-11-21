7bb75a572821fb3a9cca7781c23901fd75cff637
Lukas Rutkauskas <lukas.rutkauskas@stud.techin.lt>
2023-11-21 12:53:55 +0200

    created two empty functions randomIMG and randomProverb, also added two arrays for it

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = []; // An array holding at least 10 pictures 
+let proverb = []; // An array holding at least 10 proverbs
+
+function randomIMG (img) {
+    
+}
+function randomProverb (proverb) {
+
+}
